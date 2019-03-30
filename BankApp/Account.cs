8eccb377f19d04c67fdefa773bc894393c6f5d24
Administrator <palki3289@gmail.com>
2019-03-30 10:31:57 -0700

    Initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// Account that represents
+    /// bank account here you can
+    /// withdraw or deposit money
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique number for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// email address of the account holder
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public string AccountType { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.AccountNumber = 12345;
+            account.Balance = 10000000000000;
+        }
+    }
+}
